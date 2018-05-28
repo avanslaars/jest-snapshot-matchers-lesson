6c004e7707b2c9cddcf043fa80bda59773084785
Andrew Van Slaars <avanslaars@gmail.com>
2018-05-28 10:34:54 -0400

    Matchers added to snapshot test

--- index.spec.js
+++ index.spec.js
@@ -3,6 +3,9 @@ const { createPerson } = require('./index')
 describe('createPerson', () => {
   it('Creates a person object', () => {
     const result = createPerson('John', 'Smith')
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchSnapshot({
+      id: expect.any(String),
+      createdAt: expect.any(Date)
+    })
   })
 })
